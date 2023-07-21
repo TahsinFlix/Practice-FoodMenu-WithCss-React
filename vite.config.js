cc69083b0df65cad6bb596747f18ae94cf057cee
TahsinPC_ <tahsintubepc@gmail.com>
2023-07-21 03:14:07 +0000

    Update vite.config.js
    Giving this command // vite.config.js
    export default {
      base: '/Practice-FoodMenu-WithCss-React/' // Set the correct subdirectory name
    };
    In the  file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  base: '/Practice-FoodMenu-WithCss-React/' // Set the correct subdirectory name
+
 })
